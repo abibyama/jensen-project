8441e1363ed67d32753204ede48e673a311b3fea
Abdullahi M. Ibrahim <abdullah98@hotmail.se>
2022-03-30 15:16:19 +0200

    React router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-
 import './App.css';
-import Kurser from './componnents/AMI/Kurser';
-import Utbildningar from './componnents/MAA/Utbildningar';
+import { Link } from "react-router-dom";
+
 
 
 function App() {
   return (
     <div className="App">
-      <Utbildningar />
-      <Kurser />
+      <h1>Välkommna till Chai Academy</h1>
+      <Link to="/utbildningar"><button>Utbildningar</button></Link>
+      <Link to="/ansokan"><button>Ansök</button></Link>  
+      <Link to="/kurser"><button>Kurser</button></Link>
     </div>
   );
 }
