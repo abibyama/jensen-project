40b1254b4052b7af3def0f04fc2496be5ccf83a9
Abdullahi M. Ibrahim <abdullah98@hotmail.se>
2022-03-30 17:04:06 +0200

    Submit knapp

--- src/componnents/AMI/Form.js
+++ src/componnents/AMI/Form.js
@@ -1,45 +1,47 @@
-import React from 'react'
+  import React from 'react'
 
-function Form() {
-  return  (
-    <div className='ansökan'>
-      <h1>Lägg till kurs</h1>
-      <form>
+  function Form() {
+    return  (
+      <div className='ansökan'>
+        <h1>Lägg till kurs</h1>
+        <form>
 
-        <label>Kursnamn</label>
-                <input 
-                  type="text"
-                  required
-                />
-        <label>Ansvarig lärare</label>
-        <select>
-          <option value="Mohammad Ali">Mohammad Ali</option>
-          <option value="Per T">Per T</option>
-          <option value="Mohamed Abdi Ali">Mohamed Abdi Ali</option>
-        </select>
+          <label>Kursnamn</label>
+                  <input 
+                    type="text"
+                    required
+                  />
+          <label>Ansvarig lärare</label>
+          <select>
+            <option value="Mohammad Ali">Mohammad Ali</option>
+            <option value="Per T">Per T</option>
+            <option value="Mohamed Abdi Ali">Mohamed Abdi Ali</option>
+          </select>
 
-        <label>Kurslängd</label>
-        <select>
-          <option value="52">52 veckor</option>
-          <option value="40">40 veckor</option>
-          <option value="20">20 veckor</option>
-        </select>
+          <label>Kurslängd</label>
+          <select>
+            <option value="52">52 veckor</option>
+            <option value="40">40 veckor</option>
+            <option value="20">20 veckor</option>
+          </select>
 
+
+          <textarea
+          placeholder='Kursbeskrivning'
+          required
+          ></textarea>
+
+        <input type="submit" value="Ny kurs">Lägg till kurs</input>
+          
+
+
+
+        </form>
         
-        <textarea
-        placeholder='Kursbeskrivning'
-        required
-        ></textarea>
-        
 
-
-
-      </form>
       
-
-     
-    </div>
-    
+      </div>
+      
 
  
   )
